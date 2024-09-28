d05ef7d10ba14742748897047716260ecfc02052
Vedang Maheshwari <98143931+vedang1@users.noreply.github.com>
2024-09-28 13:51:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// app.js
+
 const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth.js');
 const trainRoutes = require('./routes/train.js');
 const bookingRoutes = require('./routes/booking.js');
 
-// Initialize environment variables
+
 dotenv.config();
 
 const app = express();
@@ -13,12 +13,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Parse JSON requests
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/trains', trainRoutes);
 app.use('/api/bookings', bookingRoutes);
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
